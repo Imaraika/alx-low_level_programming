2e23e36cfccbd12209486f51d92186ec762e239f
Ange <42787866+Imaraika@users.noreply.github.com>
2023-03-16 18:54:40 -0700

    commit message

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
 #include<stdio.h>
-#
+/**
+ * main - entry point
+ *
+ * return: 1 (fail)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+return (1);
+}
