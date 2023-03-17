3f760b59f7131ab17ac3e186ddb819efcb6018c5
Ange <42787866+Imaraika@users.noreply.github.com>
2023-03-16 19:03:09 -0700

    commit message

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
+#include<unstd.h>
 /**
- * main - entry point
- *
- * return: 1 (fail)
+ * main - c progrm thats prints 
+ * return: 1 (success)
  */
 int main(void)
 {
-fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+fputs("and that piece of art is useful\n" - Dora Korpar, 2015-10-19");
 return (1);
-}
+
