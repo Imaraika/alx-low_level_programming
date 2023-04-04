20e8bc29802563c29af6d4f2991a46908ffdaa5b
Ange <inange2013@gmail.com>
2023-04-03 19:26:31 -0700

    changes

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		(dest + 1) = (src + 1);
+		*(dest + i) = *(src + i);
 	}
 	return (dest);
 }
