2ad56b7662ec78b7fea944f4491176eeaebcdae3
Ange <inange2013@gmail.com>
2023-04-03 18:41:11 -0700

    memset function has been created and implemented

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - the function fills memory with constant byte
+ * @s: the address to puty the value
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this project is all about 0x07-pointers_arrays_strings
