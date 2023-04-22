65004bdd90bb9dd43ff58a26ca867be4584d38d9
Ange <inange2013@gmail.com>
2023-04-22 15:57:01 -0700

    library has been created

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @num: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int nbr)
+{
+	if (nbr >= 0)
+	{
+		return (nbr);
+	}
+	return (-nbr);
+}
