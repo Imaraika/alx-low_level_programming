3e2c91cf1754321919cc7047525f9c8b6dae7fa0
Ange <inange2013@gmail.com>
2023-04-25 22:42:50 -0700

    task done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0;
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		count++;
+	}
+	(void) argv; /*Ignore argv*/
+	printf("%d\n", count);
+	return (0);
+}
