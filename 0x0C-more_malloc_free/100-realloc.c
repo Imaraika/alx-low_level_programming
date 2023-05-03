80b6f85a15582b95ecb2bd6f5e73f06fd98d1abd
Ange <inange2013@gmail.com>
2023-05-03 16:22:23 -0700

    updates

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,42 +11,40 @@
  * @new_size: new size of the new memory block
  * Return: pointer to the newly allocated memory block
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
-
-	/* If new size is zero, free the memory and return NULL */
-	if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
-	/* If ptr is NULL, equivalent to malloc(new_size) */
-	if (ptr == NULL)
-	{
-		return malloc(new_size);
-	}
-
-	/* Allocate new memory block */
-	new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
-	{
-		return (NULL);
-	}
-
-	/* Copy the contents of the old block to the new block */
-	if (new_size < old_size)
-	{
-		memcpy(new_ptr, ptr, new_size);
-	}
-	else
-	{
-		memcpy(new_ptr, ptr, old_size);
-	}
-
-	/* Free the old block */
-	free(ptr);
-	return (new_ptr);
+    void *new_ptr;
+    unsigned int copy_size;
+    
+    /* If new_size is zero, free the memory block and return NULL */
+    if (new_size == 0) {
+        free(ptr);
+        return NULL;
+    }
+    
+    /* If ptr is NULL, allocate a new memory block with malloc */
+    if (ptr == NULL) {
+        new_ptr = malloc(new_size);
+        return new_ptr;
+    }
+    
+    /* If new_size is equal to old_size, do nothing and return ptr */
+    if (new_size == old_size) {
+        return ptr;
+    }
+    
+    /* Allocate a new memory block with malloc */
+    new_ptr = malloc(new_size);
+   
+    /* Determine the size to copy */
+    copy_size = old_size < new_size ? old_size : new_size;
+    
+    /* Copy the contents of the old memory block to the new memory block */
+    memcpy(new_ptr, ptr, copy_size);
+    
+    /* Free the old memory block */
+    free(ptr);
+    
+    return new_ptr;
 }
+
